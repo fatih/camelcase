6ed5a66c8d72fa5959db8d63a597af8c8de027d4
Peter Moore <petemoore@gmx.net>
2016-03-18 06:57:12 +0100

    Updated test to show original string next to split result

--- camelcase_test.go
+++ camelcase_test.go
@@ -23,25 +23,25 @@ func ExampleSplit() {
 		"Two  spaces",
 		"BadUTF8\xe2\xe2\xa1",
 	} {
-		fmt.Printf("%#v\n", Split(c))
+		fmt.Printf("%#v => %#v\n", c, Split(c))
 	}
 
 	// Output:
-	// []string{}
-	// []string{"lowercase"}
-	// []string{"Class"}
-	// []string{"My", "Class"}
-	// []string{"My", "C"}
-	// []string{"HTML"}
-	// []string{"PDF", "Loader"}
-	// []string{"A", "String"}
-	// []string{"Simple", "XML", "Parser"}
-	// []string{"vim", "RPC", "Plugin"}
-	// []string{"GL", "11", "Version"}
-	// []string{"99", "Bottles"}
-	// []string{"May", "5"}
-	// []string{"BFG", "9000"}
-	// []string{"Böse", "Überraschung"}
-	// []string{"Two", "  ", "spaces"}
-	// []string{"BadUTF8\xe2\xe2\xa1"}
+	// "" => []string{}
+	// "lowercase" => []string{"lowercase"}
+	// "Class" => []string{"Class"}
+	// "MyClass" => []string{"My", "Class"}
+	// "MyC" => []string{"My", "C"}
+	// "HTML" => []string{"HTML"}
+	// "PDFLoader" => []string{"PDF", "Loader"}
+	// "AString" => []string{"A", "String"}
+	// "SimpleXMLParser" => []string{"Simple", "XML", "Parser"}
+	// "vimRPCPlugin" => []string{"vim", "RPC", "Plugin"}
+	// "GL11Version" => []string{"GL", "11", "Version"}
+	// "99Bottles" => []string{"99", "Bottles"}
+	// "May5" => []string{"May", "5"}
+	// "BFG9000" => []string{"BFG", "9000"}
+	// "BöseÜberraschung" => []string{"Böse", "Überraschung"}
+	// "Two  spaces" => []string{"Two", "  ", "spaces"}
+	// "BadUTF8\xe2\xe2\xa1" => []string{"BadUTF8\xe2\xe2\xa1"}
 }
